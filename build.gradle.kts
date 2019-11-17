8353a1d3fc9a12b85546d15067976537e2715116
hhao99 <hhao99@163.com>
2019-11-17 17:50:10 +0800

    spark app template

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ plugins {
 dependencies {
     
     implementation("org.apache.spark:spark-core_2.12:2.4.4")
+ //   implementation("org.apache.hadoop:hadoop-client:3.1.1")
     implementation("org.apache.spark:spark-sql_2.12:2.4.4")
     
 }
--- src/main/scala/com/vgc/App.scala
+++ src/main/scala/com/vgc/App.scala
@@ -7,34 +7,13 @@ import org.apache.spark.sql.functions._
 object App {
     def main(args: Array[String]) {
         val spark = SparkSession
-      .builder()
-      .appName("Spark SQL basic example")
-      .config("spark.some.config.option", "some-value")
-      .master("local")
-      .getOrCreate()
+          .builder()
+          .appName("Spark SQL basic example")
+          .config("spark.some.config.option", "some-value")
+          .master("local")
+          .getOrCreate()
 
-        val file="file:///d:/data/bank-full.csv"
-        
-        log2(" spark " + spark.version)
-    
-        val bank = spark.read
-            .option("delimiter",";")
-            .option("header",true)
-            .option("inferSchema", "true")
-            .csv(file)
-        
-        log2("total count: " +bank.count())
-        
-        import spark.implicits._
+        val sc = spark.sparkContext
 
-        val bank2 = bank
-
-      spark.stop()
-    }
-
-    def log2(msg: String) {
-        println("===============")
-        println(msg)
-        println("===============")
     }
 }
